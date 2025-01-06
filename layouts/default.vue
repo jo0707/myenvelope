f1123dc15408e14fff69eb9fcacdf37beb4805f6
jo0707 <josua123690707@gmail.com>
2025-01-07 00:52:51 +0700

    fix: about dialog background

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,20 +21,20 @@
         </footer>
 
         <UModal v-model="isCreditOpen">
-            <UCard class="relative shadow-lg">
+            <UCard class="relative shadow-lg overflow-hidden">
                 <img src="/img/leaves.svg"
                     class="absolute top-0 left-0 object-cover scale-150 w-full h-full opacity-5 z-0" />
                 <div class="relative z-10 text-xs text-center flex flex-col gap-1">
                     <p>Made with ❤ by Jo</p>
                     <i class="text-gray-400">My gratitude to <span
-                            class="bg-gray-400 transition select-none hover:bg-transparent hover:text-opacity-20 rounded">Loxky</span>
+                            class="bg-gray-400 transition select-none hover:bg-transparent hover:opacity-20 rounded">Loxky</span>
                         for being my inspiration to build this ツ.</i>
 
                     <div class="mt-4 flex place-content-center gap-8">
                         <UButton variant="outline" color="gray" target="_blank" icon="i-mdi-github" label="jo0707"
                             size="2xs" to="https://github.com/jo0707" />
                         <UButton variant="outline" color="gray" target="_blank" icon="i-heroicons-window" label="Jo?"
-                            size="2xs" to="https://jooo.my.id/" />
+                            size="2xs" to="https://jooo.site/" />
                     </div>
                 </div>
             </UCard>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,65 +1,61 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  ssr: false,
+    devtools: { enabled: true },
+    ssr: false,
 
-  modules: [
-    "@nuxt/ui",
-    "@nuxtjs/google-fonts",
-    "@pinia/nuxt",
-    "@vueuse/nuxt",
-    "nuxt-mongoose",
-    "nuxt-security",
-    "@nuxtjs/i18n",
-  ],
-
-  colorMode: {
-    preference: "dark",
-  },
-
-  googleFonts: {
-    families: {
-      "Ubuntu Mono": true,
-      Poppins: true,
-      Tangerine: true,
-    },
-  },
-
-  ui: {
-    icons: ["mdi"],
-  },
-
-  mongoose: {
-    uri: process.env.MONGODB_URI,
-    options: {},
-    modelsDir: "models",
-  },
-
-  routeRules: {
-    "/api/envelope": {
-      security: {
-        rateLimiter: {
-          tokensPerInterval: 5,
-          interval: 10000,
-        },
-      },
-    },
-  },
-
-  i18n: {
-    strategy: "prefix_except_default",
-    defaultLocale: "en",
-    locales: [
-      {
-        code: "en",
-        name: "English",
-      },
-      {
-        code: "id",
-        name: "Indonesia",
-      },
+    modules: [
+        "@nuxt/ui",
+        "@nuxtjs/google-fonts",
+        "@pinia/nuxt",
+        "@vueuse/nuxt",
+        "nuxt-mongoose",
+        "nuxt-security",
+        "@nuxtjs/i18n",
     ],
-  },
 
-  compatibilityDate: "2025-01-06",
+    colorMode: {
+        preference: "dark",
+    },
+
+    googleFonts: {
+        families: {
+            "Ubuntu Mono": true,
+            Poppins: true,
+            Tangerine: true,
+        },
+    },
+
+    mongoose: {
+        uri: process.env.MONGODB_URI,
+        options: {},
+        modelsDir: "models",
+    },
+
+    routeRules: {
+        "/api/envelope": {
+            security: {
+                rateLimiter: {
+                    tokensPerInterval: 5,
+                    interval: 10000,
+                },
+            },
+        },
+    },
+
+    i18n: {
+        strategy: "prefix_except_default",
+        defaultLocale: "en",
+        locales: [
+            {
+                code: "en",
+                name: "English",
+            },
+            {
+                code: "id",
+                name: "Indonesia",
+            },
+        ],
+    },
+
+    compatibilityDate: "2025-01-06",
 })
